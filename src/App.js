96ec50adbddbb5eebc37639945681b75233e2180
Tom <wilf06@hotmail.co.uk>
2021-03-18 13:50:09 +0100

    text handling plus button removal

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import "./App.css";
 const axios = require("axios");
 
 function App() {
-	const [text, setText] = useState([]);
+	const [text, setText] = useState([""]);
+	const [typedText, setTypedtext] = useState([]);
 
 	useEffect(() => {
 		console.log("useEffect running");
@@ -14,12 +15,12 @@ function App() {
 			// console.log("API call complete: ", response.data);
 			let wordArray = response.data.split(" ");
 			if (wordArray.length >= 100) {
-				console.log("length is over 100: ", wordArray);
+				// console.log("length is over 100: ", wordArray);
 				wordArray.splice(100);
-				console.log("filter text: ", wordArray.join(" "));
+				// console.log("filter text: ", wordArray.join(" "));
 				setText(wordArray.join(" "));
 			} else {
-				console.log("response data is : ", response.data);
+				// console.log("length is under 100 : ", wordArray);
 				setText(response.data);
 			}
 		});
@@ -27,8 +28,16 @@ function App() {
 
 	const handleStart = (e) => {
 		console.log("click event pressed");
+		document.getElementById("start-btn").remove();
 		window.addEventListener("keydown", (e) => {
-			console.log("key pressed: ", e.key);
+			// console.log("key pressed: ", e);
+			// console.log("key pressed length: ", e.key.length);
+			if (e.key.length === 1) {
+				setTypedtext((typedText) => [...typedText, e.key]);
+			} else if (e.keyCode === 8) {
+				console.log("backspace hit");
+			}
+			console.log("typed text after update: ", typedText);
 		});
 	};
 
@@ -40,14 +49,18 @@ function App() {
 	// const handleKey = (e) => {
 	// 	console.log("key pressed is: ", e);
 	// };
-
+	console.log("typed text after update: 2 ", typedText);
 	return (
 		<div className="App">
 			<h1>Welcome to the typing speed test!</h1>
 			<h2>Type the text below to get your statistics</h2>
 			<p id="test-text">{text}</p>
-			<button onClick={handleStart}>Ready to start?</button>
+			<button id="start-btn" onClick={handleStart}>
+				Ready to start?
+			</button>
 			<button onClick={handleNewText}>Change text</button>
+			<br></br>
+			<p>{typedText}</p>
 		</div>
 	);
 }
